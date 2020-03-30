eac9afdfdf800d154f1fc04524400f9f9d332728
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2020-03-30 13:35:42 +0200

    .

--- Jegmezo/src/jegmezo/avatars/Researcher.java
+++ Jegmezo/src/jegmezo/avatars/Researcher.java
@@ -6,13 +6,8 @@ public class Researcher extends Avatar{
 
 	public Researcher() {
 		super(4); 
-		// TODO Auto-generated constructor stub
 	}
 	
-	/*
-	 * A d iranyu mezot leelenorzi és visszater annak kapacitas ertekevel
-	 * Levente
-	 */
 	public int specialMove(Direction d) throws IOException {
 		System.out.println("<Researcher.checkField()");
 		
