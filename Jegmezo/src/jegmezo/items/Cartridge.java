ff5d85c4b3f7c0152387ff7679cd1fe97453daeb
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2020-03-30 11:59:04 +0200

    upgrade skeleton

--- Jegmezo/src/jegmezo/items/Cartridge.java
+++ Jegmezo/src/jegmezo/items/Cartridge.java
@@ -6,8 +6,19 @@ public class Cartridge extends Item{
 
 	@Override
 	public void use(Avatar a) {
-		System.out.println("Cartridge.use()");
-		System.out.println(">Cartridge.use()"); 
+		System.out.println("<Gun.use()");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Ezen a fielden áll a többi alkatrész birtokosa is? Y/N");
+		String answer = null;
+    	try {
+			answer = br.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	if(answer.toUpperCase() == "Y")
+    		a.gameEnder.endGame();
+		System.out.println(">Gun.use()");
 		
 	}
 
