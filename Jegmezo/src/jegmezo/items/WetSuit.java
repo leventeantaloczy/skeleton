3507c492a48e45282b51d4ecb59732b77b7e2c0a
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2020-03-30 13:26:36 +0200

    .

--- Jegmezo/src/jegmezo/items/WetSuit.java
+++ Jegmezo/src/jegmezo/items/WetSuit.java
@@ -3,15 +3,10 @@ package jegmezo.items;
 import jegmezo.avatars.Avatar;
 
 public class WetSuit extends Item{
-	/*
-	 * Ha use-olod, beállítódik, hogy viseled. 
-	 * Innentől a dieByWater() nem bánthat.
-	 * Zoli
-	 */
+	
 	@Override
 	public void use(Avatar a) {
 		System.out.println("<WetSuit.use()");
-		a.wearsWetsuit = true;
 		System.out.println(">WetSuit.use()");
 		
 	}
