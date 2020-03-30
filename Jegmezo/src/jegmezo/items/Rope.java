4e1d5b02768d3476a62440f2ea01fd937ab879b0
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2020-03-30 13:28:08 +0200

    .

--- Jegmezo/src/jegmezo/items/Rope.java
+++ Jegmezo/src/jegmezo/items/Rope.java
@@ -11,30 +11,34 @@ import jegmezo.fields.Field;
 
 public class Rope extends Item{
 
+	/*
+	 * TODO a szeki nem vilagos pls help
+	 * Levente
+	 */
 	@Override
 	public void use(Avatar a) {
 		System.out.println("<Rope.use()");
 		
 		System.out.println("Which direction? There is someone to your West, but noone in any Other direction. W/O");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String dir = null;
-			try {
-				dir = br.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		if(dir.toUpperCase() == "W") {
-			Field f;
-			f = a.getField().getNeighbour(Direction.West);
-			for(Avatar av : f.avatars){
-				try {
+    	String dir = null;
+		try {
+			dir = br.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	if(dir.toUpperCase() == "W") {
+    		Field f;
+    		f = a.getField().getNeighbour(Direction.West);
+    		for(Avatar av : f.avatars){
+    			try {
 					av.move(Direction.East);
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-			}
-		}
-		System.out.println(">Rope.use()");
+    		}
     	}
+    	System.out.println(">Rope.use()");
+    }
 }
