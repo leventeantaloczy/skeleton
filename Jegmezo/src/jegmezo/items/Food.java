160a23d5b8c4f997016f5e6155590e6876e873c0
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2020-03-30 13:30:08 +0200

    .

--- Jegmezo/src/jegmezo/items/Food.java
+++ Jegmezo/src/jegmezo/items/Food.java
@@ -4,11 +4,6 @@ import jegmezo.avatars.Avatar;
 
 public class Food extends Item{
 
-	/*
-	 * TODO aktivitas???
-	 * Eszik es felmegy az elete
-	 * Levente
-	 */
 	@Override
 	public void use(Avatar a) {
 		System.out.println("<Food.use()");
