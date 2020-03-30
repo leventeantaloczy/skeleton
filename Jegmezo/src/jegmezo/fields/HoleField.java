724ff7e0419ea0b78165ec53d0a5d8aa2bcf9f7e
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2020-03-30 10:54:04 +0200

    skeleton upgrade

--- Jegmezo/src/jegmezo/fields/HoleField.java
+++ Jegmezo/src/jegmezo/fields/HoleField.java
@@ -1,5 +1,10 @@
 package jegmezo.fields;
 
 public class HoleField extends Field{
-	
+	public boolean addAvatar(Avatar a) {
+		System.out.println("<Field.addAvatar()");
+		avatars.add(a);
+		System.out.println(">Field.addAvatar()");
+		return true;
+	}
 }
