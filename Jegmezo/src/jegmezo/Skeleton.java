38921b97f38842539659705d78a3672f9b6d80da
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2020-03-30 13:25:57 +0200

    update

--- Jegmezo/src/jegmezo/Skeleton.java
+++ Jegmezo/src/jegmezo/Skeleton.java
@@ -5,36 +5,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-import jegmezo.avatars.Eskimo;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Skeleton {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
+		System.out.println("What should we test?");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Program inditasa:");
-		
-		while(true) {
-			System.out.println("Jatek inicializalas: 1");
-			System.out.println("Avatar mozgatása: 2");
-			System.out.println("Eskimo iglut epit: 3");
-			System.out.println("Sarkkutato megnezi a szomszedos jegtablat: 4");
-			
-			switch(br.readLine()) {
-			case("1"):{
-				Controller controller = new Controller();
-				controller.startGame();
-			}
-			case("2"):{
-				GameEnder gameEnder = new GameEnder();
-				GameArea gameArea = new GameArea(gameEnder);
-				Eskimo eskimo = new Eskimo();
-				gameArea.addAvatar(eskimo);
-				
-				
-			}
-			}
-			
+        System.out.println("Init (Start Game): 1; Avatar move (Field accept, Add Avatar, Remove Avatar, Set Activity Points): 2;");
+        System.out.println("Igloo building (Set Activity Points): 3; Check capacity (Set Activity Points): 4;");
+        System.out.println("Avatar dies in water (EndGame): 5; Avatar dies because of heat loss (EndGame): 6");
+        System.out.println("Backpacking and using an Item (Set Activity Points): 7 or a Snow Storm: 8 ?");
+        String command = null;
+		try {
+			command = br.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	}
-
+		Controller c = new Controller(command);
+    }
 }
