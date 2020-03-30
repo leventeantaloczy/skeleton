479f9c50d78d64ace283f843167adffd99dbde28
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2020-03-30 11:58:11 +0200

    upgrade skeleton

--- Jegmezo/src/jegmezo/items/Gun.java
+++ Jegmezo/src/jegmezo/items/Gun.java
@@ -7,6 +7,17 @@ public class Gun extends Item{
 	@Override
 	public void use(Avatar a) {
 		System.out.println("<Gun.use()");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Ezen a fielden áll a többi alkatrész birtokosa is? Y/N");
+		String answer = null;
+    	try {
+			answer = br.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	if(answer.toUpperCase() == "Y")
+    		a.gameEnder.endGame();
 		System.out.println(">Gun.use()");
 		
 	}
